0b6eb13903b3057b12341af8015d7044ecb58a3f
Diego Nunes <diego.nunes@pricefy.com.br>
2021-07-21 20:21:10 -0300

    Inserindo a documentação em ongs e especies

--- routes/especies.js
+++ routes/especies.js
@@ -3,6 +3,52 @@ const especie = require('../models/especies');
 
 const router = new express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     especies:
+ *       type: object
+ *       required:
+ *         - nome
+ *       properties:
+ *         nome:
+ *           type: string
+ *           description: descrição da espécie
+ *       example:
+ *         _id: 60e9fbd0c71254fd715ba5f1,
+ *         nome: Cachorro
+ */
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Especies
+  *   description: Espécies dos animais
+  */
+
+/**
+ * @swagger
+ * /especies:
+ *   post:
+ *     summary: Adiciona uma nova espécie
+ *     tags: [Especies]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/especies'
+ *     responses:
+ *       200:
+ *         description: Espécie inserida com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/especies'
+ *       500:
+ *         description: Não foi possível inserir a espécie
+ */
 router.post('/especies', async (req, res) => {
     const addEspecie = new especie(req.body);
     try {
@@ -13,6 +59,22 @@ router.post('/especies', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /especies:
+ *   get:
+ *     summary: Retorna todos as espécies
+ *     tags: [Especies]
+ *     responses:
+ *       200:
+ *         description: Lista todas as espécies
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/especies'
+ */
 router.get('/especies', async (req, res) => {
     try {
         const getEspecies = await especie.find({});
@@ -22,6 +84,31 @@ router.get('/especies', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /especies/{id}:
+ *   get:
+ *     summary: Recupera uma espécie pelo id
+ *     tags: [Especies]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: id da espécie
+ *     responses:
+ *       200:
+ *         description: Espécie pelo id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/especies'
+ *       404:
+ *         description: Espécie não encontrada
+ *       500:
+ *         description: Não foi possível listar a espécie
+ */
 router.get('/especies/:id', async (req, res) => {
     const _id = req.params.id;
     try {
--- routes/ongs.js
+++ routes/ongs.js
@@ -3,6 +3,84 @@ const ong = require('../models/ongs');
 
 const router = new express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     ongs:
+ *       type: object
+ *       required:
+ *         - nome
+ *         - cnpj
+ *         - telefone
+ *         - email
+ *         - endereco
+ *         - data_cadastro
+ *       properties:
+ *         nome:
+ *           type: string
+ *           description: nome da ong
+ *         cnpj:
+ *           type: string
+ *           description: cnpj da ong
+ *         telefone:
+ *           type: string
+ *           description: numero de telefone
+ *         email:
+ *           type: string
+ *           description: e-mail
+ *         facebook:
+ *           type: string
+ *           description: nome da ong
+ *         instagram:
+ *           type: string
+ *           description: cnpj da ong
+ *         endereco:
+ *           type: string
+ *           description: numero de telefone
+ *         data_cadastro:
+ *           type: Date
+ *           description: e-mail
+ *       example:
+ *         nome: Lar Feliz
+ *         cnpj: 00.000.000/0001-00
+ *         telefone: (35) 90000-0000
+ *         email: onglarfeliz@gmail.com
+ *         facebook: larfeliz
+ *         instagram: larfelizoficial
+ *         endereco: Rua da Paz, 41, centro - Conceição dos Ouros - MG
+ *         data_cadastro: 2021-01-01
+ */
+
+/**
+  * @swagger
+  * tags:
+  *   name: Ongs
+  *   description: Ongs de proteção de animais
+  */
+
+/**
+ * @swagger
+ * /ongs:
+ *   post:
+ *     summary: Adiciona uma nova ong
+ *     tags: [Ongs]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ongs'
+ *     responses:
+ *       200:
+ *         description: Ong inserida com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ongs'
+ *       500:
+ *         description: Não foi possível inserir a ong
+ */
 router.post('/ongs', async (req, res) => {
     const addOng = new ong(req.body);
     try {
@@ -13,6 +91,22 @@ router.post('/ongs', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /ongs:
+ *   get:
+ *     summary: Retorna todos as ongs
+ *     tags: [Ongs]
+ *     responses:
+ *       200:
+ *         description: Lista todas as ongs
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/ongs'
+ */
 router.get('/ongs', async (req, res) => {
     try {
         const getOngs = await ong.find({});
@@ -22,6 +116,31 @@ router.get('/ongs', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /ongs/{id}:
+ *   get:
+ *     summary: Recupera uma ong pelo id
+ *     tags: [Ongs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: id da ong
+ *     responses:
+ *       200:
+ *         description: Ong
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ongs'
+ *       404:
+ *         description: Ong não encontrada
+ *       500:
+ *         description: Não foi possível listar a ong
+ */
 router.get('/ongs/:id', async (req, res) => {
     const _id = req.params.id;
     try {
@@ -37,6 +156,39 @@ router.get('/ongs/:id', async (req, res) => {
     }
 } )
 
+/**
+ * @swagger
+ * /ongs/{id}:
+ *  patch:
+ *    summary: Altera uma ong
+ *    tags: [Ongs]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: id da ong
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/ongs'
+ *    responses:
+ *      200:
+ *        description: Ong alterada com sucesso
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/ongs'
+ *      400:
+ *        description: Não foi possivel alterar algum campo especifico
+ *      404:
+ *        description: Ong não encontrada
+ *      500:
+ *        description: Não foi possível alterar a ong
+ */
 router.patch('/ongs/:id', async (req, res) => {    
     const dataUpdate = Object.keys(req.body);
     const allowedUpdate = ['nome', 'telefone', 'email', 'facebook', 'instagram', 'endereco'];
@@ -61,6 +213,28 @@ router.patch('/ongs/:id', async (req, res) => {
     }
 } )
 
+/**
+ * @swagger
+ * /ongs/{id}:
+ *   delete:
+ *     summary: Remove uma ong
+ *     tags: [Ongs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: id da ong
+ * 
+ *     responses:
+ *       200:
+ *         description: Ong removida com sucesso
+ *       404:
+ *         description: Ong não encontrada
+ *       500:
+ *         description: Não foi possível deletar a Ong
+ */
 router.delete('/ongs/:id', async (req, res) => {    
     try {
         const deleteOng = await ong.findByIdAndDelete(req.params.id);
