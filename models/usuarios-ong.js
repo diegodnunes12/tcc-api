2e1b98de87ea59048f3714bcbcf9194cfa14a620
Diego Nunes <diegodnunes12@gmail.com>
2021-08-25 20:06:30 -0300

    Removendo cpf do usuário

--- models/usuarios-ong.js
+++ models/usuarios-ong.js
@@ -11,14 +11,6 @@ var usuarioOngSchema = new Schema ({
         maxlength: 50,
         trim: true
     },
-    cpf: {
-        type: String,
-        required:false,
-        maxlength: 15,
-        trim: true,
-        index: true,
-        unique: true
-    },
     email: {
         type: String,
         required:true,
