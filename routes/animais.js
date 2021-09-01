339f71b19f457039de4db6f4e782e9f34ec67465
Diego Nunes <diegodnunes12@gmail.com>
2021-09-01 20:25:52 -0300

    Corringindo bug de busca

--- routes/animais.js
+++ routes/animais.js
@@ -244,7 +244,7 @@ router.get('/animais', (req, res) => {
 
         animal.find({}, {}, (err, data) => {
             animal.countDocuments((err, qtd) => {
-                res.status(200).send({ paginaAtual: page, paginas: Math.ceil(qtd/limit), total: qtd, data })
+                res.status(200).send({ paginaAtual: page, paginas: Math.ceil(qtd/limit), total: qtd, animais: data })
             })
         }).limit(limit * 1).skip((page - 1) * limit).populate("especie").populate("porte").populate("ong");        
     } catch (error) {
@@ -416,9 +416,9 @@ router.get('/animais/filtro', (req, res) => {
                             }
                         });
                     }                    
-                    res.status(200).send({ paginaAtual: page, paginas: Math.ceil(qtd/limit), total: animais.length, animais })
+                    res.status(200).send({ paginaAtual: page, paginas: Math.ceil(qtd/limit), total: animais.length, animais: animais })
                 } else {
-                    res.status(200).send({ paginaAtual: page, paginas: Math.ceil(qtd/limit), total: data.length, data })
+                    res.status(200).send({ paginaAtual: page, paginas: Math.ceil(qtd/limit), total: data.length, animais: data })
                 } 
             })
         }).populate("especie").populate("porte").populate("ong");
